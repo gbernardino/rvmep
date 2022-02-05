9c0a7a1a747bfbe319562201a072f6919a6ae1c3
Gabriel Bernardino <gabriel.bernardino1@gmail.com>
2022-02-05 16:32:41 +0100

    rvmep/volumetricParcellation.py

--- rvmep/tools.py
+++ rvmep/tools.py
@@ -1,5 +1,5 @@
 import numpy as np, pyvista
-from vtk.util.numpy_support import vtk_to_numpy
+from vtk.util.numpy_support import vtk_to_numpy, numpy_to_vtk
 
 def read_ucd_to_vtk(path):
     """
@@ -33,19 +33,25 @@ def read_ucd_to_vtk(path):
             elif iPointsData < nPoints:
                 pointData[iPointsData] = list(map(float, l.split()[-2 : ]))
                 iPointsData += 1
-    return pyvista.PolyData(points,  np.concatenate((3*np.ones([nFaces, 1], dtype = np.uint16) , triangles), axis = 1).flatten())
+    return numpyToPyvista(points, triangles)
 
 def getTomtecApexValvePointsRV():
     """
     Hand selected points from the TOMTEC RV model
     """
-    pointsTricuspid = [388, 389, 392, 393, 144, 540, 145, 538, 539, 422, 423, 38, 541, 49, 55, 328, 329, 332, 333, 87, 94, 100, 101, 103, 104, 105, 122, 123, 126, 127]
-    pointsPulmonary = [410, 411, 409, 408, 53, 64, 65, 66, 67, 68, 69, 83, 476, 477, 92, 478, 480, 481, 482, 483, 484, 485, 486, 487, 488, 489, 479]
+    pointsTricuspid = np.array([388, 389, 392, 393, 144, 540, 145, 538, 539, 422, 423, 38, 541, 49, 55, 328, 329, 332, 333, 87, 94, 100, 101, 103, 104, 105, 122, 123, 126, 127])
+    pointsPulmonary = np.array([410, 411, 409, 408, 53, 64, 65, 66, 67, 68, 69, 83, 476, 477, 92, 478, 480, 481, 482, 483, 484, 485, 486, 487, 488, 489, 479])
     apexId = 906
-    return apexId, np.array(pointsTricuspid + pointsPulmonary)
+    return apexId, pointsTricuspid, pointsPulmonary
+
+def numpyToPyvista(points, triangles):
+    return pyvista.PolyData(points,  np.concatenate((3*np.ones([len(triangles), 1], dtype = np.uint16) , triangles), axis = 1).flatten())
+
+def pyvistaToNumpy(mesh):
+    return mesh.points, mesh.faces.reshape((-1, 4))[:, 1:]
 
 def addArrayToMeshVTK(mesh, array, name, domain = 'points'):
-    arrayVTK = vtk_to_numpy(array)
+    arrayVTK = numpy_to_vtk(array)
     arrayVTK.SetName(name)
 
     if domain == 'points':
