caeb62b584916f88c00da482c00391822792dd80
Gabriel Bernardino <gabriel.bernardino1@gmail.com>
2023-01-16 10:50:10 +0100

    Fixed error while computing strain

--- rvmep/computeStrain.py
+++ rvmep/computeStrain.py
@@ -3,25 +3,25 @@ import numpy as np
 def computeDeformationTensor(ref, moving):
     E = np.zeros((ref.n_cells, 3, 3))
     triangles = ref.faces.reshape((-1, 4))[:, 1:]
+    D = np.array([[-1, 1, 0], [-1, 0, 1]])
     for i, t in enumerate(triangles):
-        E[i] = np.linalg.pinv(ref.points[t])@moving.points[t]
+        E[i] = np.linalg.pinv(D@ ref.points[t]) @ D @moving.points[t]
     return E
 
 
-def computeStrainTensorGreen(ref, moving):
-    strain = np.zeros((ref.n_cells, 3, 3))
-    triangles = ref.faces.reshape((-1, 4))[:, 1:]
-    for i, t in enumerate(triangles):
-        E = np.linalg.pinv(ref.points[t])@moving.points[t]
-        strain[i] = (E.T@E - np.eye(3))/2
-    return strain
+
+
 
 def computeStrainTensorInfinitessimal(ref, moving):
     strain = np.zeros((ref.n_cells, 3, 3))
-    triangles = ref.faces.reshape((-1, 4))[:, 1:]
-    for i, t in enumerate(triangles):
-        E = np.linalg.pinv(ref.points[t])@moving.points[t]
-        strain[i] = (E.T + E)/2  - np.eye(3)
+    E = computeDeformationTensor(ref, moving)
+    for i, t in enumerate(strain):
+        strain[i] = (E[i].T + E[i])/2  - np.eye(3)
+    return strain
+
+def computeStrainTensorGreen(ref, moving):
+    E = computeDeformationTensor(ref, moving)
+    strain = (np.einsum('nji,njk->nik', E, E) - np.eye(3))/2
     return strain
 
 
